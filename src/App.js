aa12ec3068c3c4093a348ef3a952b65c6134e024
Duncan Moyo <duncanfmoyo@gmail.com>
2020-04-15 23:31:48 +0200

    card flip functioning

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
+import FlashcardList from "./FlashcardList";
 
-function App() {
+const App = () => {
+
+  const SAMPLE_FLASHCARDS = [
+    {
+      id: 1,
+      question: "what is 2 + 2 ?",
+      answer: '4',
+      options: ["2", "3", "4", "5"],
+    },
+    {
+      id: 2,
+      question: "what is 20 + 20 ?",
+      answer: '40',
+      options: ["20", "30", "40", "50"],
+    },
+    {
+      id: 3,
+      answer: '400',
+      question: "what is 200 + 200 ?",
+      options: ["200", "300", "400", "500"],
+    },
+  ];
+
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <FlashcardList flashcards={flashcards} />
+    </div>
   );
+
 }
 
 export default App;
